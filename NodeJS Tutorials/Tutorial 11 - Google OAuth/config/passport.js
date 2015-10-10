1bc1fcdacb16a6557d190854f951f1cac9b1cf45
tress <akant3k13@gmail.com>
2015-10-10 19:54:46 +0530

    Update passport.js

--- NodeJS Tutorials/Tutorial 11 - Google OAuth/config/passport.js
+++ NodeJS Tutorials/Tutorial 11 - Google OAuth/config/passport.js
@@ -1,9 +1,8 @@
 var LocalStrategy = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-var User            = require('../app/models/user');
-var configAuth = require('./auth');
+var User = require('../app/models/user'); //Mongo database
+var configAuth = require('./auth');       
 
 module.exports = function(passport) {
 
@@ -70,37 +69,7 @@ module.exports = function(passport) {
 	));
 
 
-	passport.use(new FacebookStrategy({
-	    clientID: configAuth.facebookAuth.clientID,
-	    clientSecret: configAuth.facebookAuth.clientSecret,
-	    callbackURL: configAuth.facebookAuth.callbackURL
-	  },
-	  function(accessToken, refreshToken, profile, done) {
-	    	process.nextTick(function(){
-	    		User.findOne({'facebook.id': profile.id}, function(err, user){
-	    			if(err)
-	    				return done(err);
-	    			if(user)
-	    				return done(null, user);
-	    			else {
-	    				var newUser = new User();
-	    				newUser.facebook.id = profile.id;
-	    				newUser.facebook.token = accessToken;
-	    				newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-	    				newUser.facebook.email = profile.emails[0].value;
-
-	    				newUser.save(function(err){
-	    					if(err)
-	    						throw err;
-	    					return done(null, newUser);
-	    				})
-	    				console.log(profile);
-	    			}
-	    		});
-	    	});
-	    }
-
-	));
+	
 
 	passport.use(new GoogleStrategy({
 	    clientID: configAuth.googleAuth.clientID,
