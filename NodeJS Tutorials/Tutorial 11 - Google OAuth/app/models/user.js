ca2b24648526ae8650f1b29e7477a0757a244550
tress <akant3k13@gmail.com>
2015-10-10 15:20:25 +0530

    Update user.js

--- NodeJS Tutorials/Tutorial 11 - Google OAuth/app/models/user.js
+++ NodeJS Tutorials/Tutorial 11 - Google OAuth/app/models/user.js
@@ -5,12 +5,7 @@ var userSchema = mongoose.Schema({
 		username: String,
 		password: String
 	},
-	facebook: {
-		id: String,
-		token: String,
-		email: String,
-		name: String
-	},
+	
 	google: {
 		id: String,
 		token: String,
