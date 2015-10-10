df816712e0fc4cf92352956dcfc9f95e6ba218b2
tress <akant3k13@gmail.com>
2015-10-10 21:21:05 +0530

    Update routes.js

--- NodeJS Tutorials/Tutorial 11 - Google OAuth/app/routes.js
+++ NodeJS Tutorials/Tutorial 11 - Google OAuth/app/routes.js
@@ -16,6 +16,7 @@ module.exports = function(app, passport){
 	app.get('/signup', function(req, res){
 		res.render('signup.ejs', { message: req.flash('signupMessage') });
 	});
+	
 
 
 	app.post('/signup', passport.authenticate('local-signup', {
