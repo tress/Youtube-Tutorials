6f9d7b2c1d0b99aa270975f9d1df610c3f9fcf83
tress <akant3k13@gmail.com>
2015-10-11 22:47:11 +0530

    Update user.js

--- NodeJS Tutorials/Tutorial 11 - Google OAuth/app/models/user.js
+++ NodeJS Tutorials/Tutorial 11 - Google OAuth/app/models/user.js
@@ -12,11 +12,7 @@ var userSchema = mongoose.Schema({
 		email: String,
 		name: String
 	}
-	calendar: {
-		
-		starttime:dateTime,
-		endtime:dateTime
-	}
+
 });
 
 userSchema.methods.generateHash = function(password){
