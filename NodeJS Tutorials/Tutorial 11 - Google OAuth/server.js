e194442937ac362265ae20922bbcd4aca4585c51
tress <akant3k13@gmail.com>
2015-10-12 21:16:09 +0530

    Update server.js

--- NodeJS Tutorials/Tutorial 11 - Google OAuth/server.js
+++ NodeJS Tutorials/Tutorial 11 - Google OAuth/server.js
@@ -31,14 +31,6 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.set('view engine', 'ejs');
 
-
-// app.use('/', function(req, res){
-// 	res.send('Our First Express program!');
-// 	console.log(req.cookies);
-// 	console.log('================');
-// 	console.log(req.session);
-// });
-
 require('./app/routes.js')(app, passport);
 
 app.listen(port);
